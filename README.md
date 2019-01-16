d0542d38a0866b3d0d9ac95557443e82d339517a
Pittan <pittan@pittankopta.net>
2019-01-17 00:56:08 +0900

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Awesome clipboard replace man(ACRM)
+
+# How to use
+- `npm install` or `yarn install`
+- Edit replacements inside `src/index.ts`
+- `npm start` or `yarn start`
+- Enjoy!!
+
+# Milestone
+- Make it more awesome
+- CLI Tool
+- More replacement options
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { interval, from } from 'rxjs'
+import { distinctUntilChanged, flatMap, filter, tap, map } from 'rxjs/operators'
+import * as notifier from 'node-notifier'
+import * as clipboardy from 'clipboardy'
+import * as path from 'path'
+
+const replacements = [
+  { from: 'awesome', to: 'AWESOME' },
+  { from: 'super', to: 'SUPER' }
+]
+
+interval(800)
+.pipe(
+  flatMap(x => from<string>(clipboardy.read())),
+  distinctUntilChanged(),
+  filter(x => replacements.some(r => x.includes(r.from))),
+  map(x => replace(x, replacements)),
+  tap(x => clipboardy.writeSync(x))
+).subscribe(res => {
+  notifier.notify({
+    title: 'ACRM',
+    message: 'replaced!',
+    icon: path.join(__dirname, 'icon.jpg')
+  })
+})
+
+function replace (input, replacements): string {
+  let x = input
+  replacements.forEach(r => {
+    x = x.replace(r.from, r.to)
+  })
+  return x
+}
